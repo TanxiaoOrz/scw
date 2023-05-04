3507e0ccae620af2e343852e8bcb0230c237d8ba
Tanxiao <13671985248@163.com>
2023-05-04 21:37:56 +0800

    NotificationMapper.java 通知创建方法与链接方法完成

---
+++ src/main/java/com/example/scw/mapper/NotificationMapper.java
@@ -0,0 +1,17 @@
+package com.example.scw.mapper;
+
+import com.example.scw.pojo.entity.Notification;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface NotificationMapper {
+
+    @Insert("insert into notification (NotId, Publisher, Content) VALUES (#{NotId}, #{Publisher}, #{Content})")
+    Integer newNotification(Notification notification);
+
+    @Insert("insert into not_to_user (Notification, User, Status)  (select #{NotId},UserId,0 from user where UserType = 'student')")
+    Integer linkAllStudent(@Param("NotId")Integer notId);
+
+}
