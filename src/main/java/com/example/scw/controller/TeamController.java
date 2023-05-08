ac2379915a70d9115f6950016cd0bd549d41297d
Tanxiao <13671985248@163.com>
2023-05-08 11:21:22 +0800

    Team获取接口声明，个人任务业务类声明

--- src/main/java/com/example/scw/controller/TeamController.java
+++ src/main/java/com/example/scw/controller/TeamController.java
@@ -1,11 +1,16 @@
 package com.example.scw.controller;
 
 import com.example.scw.pojo.dto.TeamConfigDto;
+import com.example.scw.pojo.dto.TeamWorkDto;
+import com.example.scw.pojo.entity.Team;
 import com.example.scw.pojo.exception.ParameterException;
 import com.example.scw.pojo.vo.Vo;
 import com.example.scw.service.TeamService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,15 +24,21 @@ public class TeamController {
     TeamService teamService;
 
     @GetMapping("/configGet")
-    @ApiOperation(value = "获取队伍设置接口",notes = "仅检查登录字符串")
+    @ApiOperation(value = "获取队伍设置接口", notes = "仅检查登录字符串")
     public Vo<TeamConfigDto> getConfig() {
         return new Vo<>(teamService.getTeamConfig());
     }
 
     @PostMapping("/configUpdate")
-    @ApiOperation(value = "修改队伍设置接口",notes = "验证教师登录\n会检查包含在数字内的成员职责名是否填写\n会检查成员数字是否超过范围")
+    @ApiOperation(value = "修改队伍设置接口", notes = "验证教师登录\n会检查包含在数字内的成员职责名是否填写\n会检查成员数字是否超过范围")
     public Vo<String> modifyConfig(@RequestBody TeamConfigDto teamConfigDto) throws ParameterException {
         return new Vo<>(teamService.updateTeamConfig(teamConfigDto));
+    }
+
+    @GetMapping("/student")
+    @ApiOperation(value = "获取所在团队信息")
+    public Vo<Team> getTeam(HttpServletRequest request) {
+        return null;
     }
 
 }
---
+++ src/main/java/com/example/scw/pojo/dto/SingleWorkDto.java
@@ -0,0 +1,7 @@
+package com.example.scw.pojo.dto;
+
+import com.example.scw.pojo.entity.SingleWork;
+
+public class SingleWorkDto extends SingleWork {
+
+}
