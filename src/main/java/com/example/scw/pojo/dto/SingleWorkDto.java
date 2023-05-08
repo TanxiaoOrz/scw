42f22831320b69d6627940efec97d8b8287d6b0e
Tanxiao <13671985248@163.com>
2023-05-08 12:09:20 +0800

    运算类描述添加

--- src/main/java/com/example/scw/pojo/dto/SingleWorkDto.java
+++ src/main/java/com/example/scw/pojo/dto/SingleWorkDto.java
@@ -1,7 +1,33 @@
 package com.example.scw.pojo.dto;
 
 import com.example.scw.pojo.entity.SingleWork;
+import com.example.scw.pojo.entity.StudyWork;
+import com.example.scw.pojo.entity.TeamWork;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value = "SingleWorkDto", description = "个人业务运算类")
 public class SingleWorkDto extends SingleWork {
+    @ApiModelProperty("从属的学习任务")
+    StudyWork studyWork;
+    @ApiModelProperty("从属的团队任务")
+    TeamWork teamWork;
+
+    public StudyWork getStudyWork() {
+        return studyWork;
+    }
+
+    public void setStudyWork(StudyWork studyWork) {
+        this.studyWork = studyWork;
+    }
+
+    public TeamWork getTeamWork() {
+        return teamWork;
+    }
+
+    public void setTeamWork(TeamWork teamWork) {
+        this.teamWork = teamWork;
+    }
 
 }
--- src/main/java/com/example/scw/pojo/dto/TeamWorkDto.java
+++ src/main/java/com/example/scw/pojo/dto/TeamWorkDto.java
@@ -2,14 +2,23 @@ package com.example.scw.pojo.dto;
 
 import com.example.scw.pojo.entity.Comment;
 import com.example.scw.pojo.entity.SingleWork;
+import com.example.scw.pojo.entity.StudyWork;
 import com.example.scw.pojo.entity.TeamWork;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
 import java.util.List;
 
+@ApiModel(value = "TeamWorkDto", description = "团队任务运算类")
 public class TeamWorkDto extends TeamWork {
 
+    @ApiModelProperty("教师评价，当已提交且教师评价提交后才会出现")
     private Comment comment;
+    @ApiModelProperty("生成的个人任务数组")
     private List<SingleWork> singleWorks;
+    @ApiModelProperty("从属的学习任务")
+    private StudyWork studyWork;
 
     public TeamWorkDto(TeamWork teamWork) {
         setTeamWorkId(teamWork.getTeamWorkId());
@@ -35,4 +44,13 @@ public class TeamWorkDto extends TeamWork {
     public void setSingleWorks(List<SingleWork> singleWorks) {
         this.singleWorks = singleWorks;
     }
+
+    public StudyWork getStudyWork() {
+        return studyWork;
+    }
+
+    public void setStudyWork(StudyWork studyWork) {
+        this.studyWork = studyWork;
+    }
+
 }
